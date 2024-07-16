534b967a71d0dc7f118353d263c81046745f3880
hongling <u202341863.xs.ustb.edu.cn>
2024-07-16 20:47:38 +0800

    aaa

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,3 +1,5 @@
+import java.awt.geom.NoninvertibleTransformException;
+
 /** Class that prints the Collatz sequence starting from a given number.
  *  @author YOUR NAME HERE
  */
@@ -5,12 +7,10 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        if (n %2==1){
+            return 3 * n + 1 ;
+        }  else {
+            return n / 2;
         }
     }
 
